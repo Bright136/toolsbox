cbb519aaa0b0959209d23f00a836bb076f40eda5
Bright Eshun <eshunbright136@gmail.com>
2023-08-08 11:12:10 +0000

    Documented and add comments to the codes for the database tables

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ db = SQLAlchemy(app, )
 # db.init_app(app)
 
 
+# Tool class represents tools in the database
 class Tool(db.Model):
+    """Class representing tools in the database."""
+
     __tablename__ = 'tools'
     tool_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -23,19 +26,27 @@ class Tool(db.Model):
     website_url = db.Column(db.String(255), nullable=True)
     pricing = db.Column(db.String(50), nullable=True)
     avg_rating = db.Column(db.Float, nullable=True)
+    # Establish a many-to-many relationship with Category using ToolCategory table
     categories = db.relationship('Category', secondary='tool_category', back_populates='tools')
+    # Establish a one-to-many relationship with Review
     reviews = db.relationship('Review', back_populates='tool')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
+# Category class represents categories in the database
 class Category(db.Model):
+    """Class representing categories in the database."""
+
     __tablename__ = 'categories'
     category_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    # Establish a many-to-many relationship with Tool using ToolCategory table
     tools = db.relationship('Tool', secondary='tool_category', back_populates='categories')
 
-
+# User class represents users in the database
 class User(db.Model):
+    """Class representing users in the database."""
+
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
@@ -43,12 +54,18 @@ class User(db.Model):
     password_hash = db.Column(db.String, nullable=False)
     access = db.Column(db.String, nullable=False)
 
+# ToolCategory class represents the association table for Tool and Category
 class ToolCategory(db.Model):
+    """Class representing the association between Tool and Category."""
+
     __tablename__ = 'tool_category'
     tool_id = db.Column(db.Integer, db.ForeignKey('tools.tool_id'), primary_key=True)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.category_id'), primary_key=True)
 
+# Review class represents reviews in the database
 class Review(db.Model):
+    """Class representing reviews in the database."""
+
     __tablename__ = 'reviews'
     review_id = db.Column(db.Integer, primary_key=True)
     tool_id = db.Column(db.Integer, db.ForeignKey('tools.tool_id'), nullable=False)
@@ -56,7 +73,9 @@ class Review(db.Model):
     rating = db.Column(db.Float, nullable=False)
     comment = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # Establish a many-to-one relationship with Tool
     tool = db.relationship('Tool', back_populates='reviews')
+
 
 
 
